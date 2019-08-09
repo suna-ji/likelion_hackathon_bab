2ec471df5cac5a130f653bd420153e88135846c1
suna <pooyo314@dongguk.edu>
2019-08-10 00:28:23 +0900

    remove cache

--- bab_project/settings.py
+++ bab_project/settings.py
@@ -151,6 +151,7 @@ LOGIN_URL = 'account_login'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bab_project', 'static')
 ]
